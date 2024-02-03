249f3fb6f0f5074133a4dec3e8c88be402ef6f2f
Akash Sarkar <akash.sarkar1489@outlook.com>
2024-02-03 20:08:43 +0530

    app components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return(
+        <footer>
+            <h4>Made With ❤️</h4>
+        </footer>
+    )
+} 
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+    return(
+        <header>
+            <nav>
+                <h2>Note MED</h2>
+            </nav>
+        </header>
+    )
+}
