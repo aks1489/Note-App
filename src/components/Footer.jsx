d9221b5b78f277333c735a2f76792570f40bdad4
Akash Sarkar <akash.sarkar1489@outlook.com>
2024-02-11 20:08:01 +0530

    dark mode toggler

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer(props) {
     return(
         <footer className={props.theme}>
-            <h4 className={props.theme}>Made With ❤️</h4>
+            <h4 className={props.theme}>Made With ❤️ ~ Akash</h4>
         </footer>
     )
 } 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,15 @@
 export default function Header(props) {
+    const theme = props.theme
+    const handleTheme = props.handleTheme
     return(
-            <nav className={props.theme}>
-                <h2 className={props.theme} onClick={props.handleTheme}>Note MED</h2>
+            <nav className={theme}>
+                <h2 className={theme} >Note MED</h2>
+                <label class="ui-switch">
+                    <input type="checkbox" checked={theme} onClick={handleTheme}/>
+                    <div class="slider">
+                        <div class="circle"></div>
+                    </div>
+                </label>
             </nav>
     )
 }
