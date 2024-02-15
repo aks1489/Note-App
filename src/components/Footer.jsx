b644de2f292405d752974e90ac0352b8d00ad04a
Akash Sarkar <akash.sarkar1489@outlook.com>
2024-02-15 13:18:29 +0530

    icon change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer(props) {
     const theme = props.theme
     return(
         <footer className={theme}>
-            <h4 className={theme}>Made With ❤️ ~ <span className={`credit ${theme}`}>Akash</span></h4>
+            <h4 className={theme}>Made With⚡ ~ <span className={`credit ${theme}`}>Akash</span></h4>
         </footer>
     )
 } 
